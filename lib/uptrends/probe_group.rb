903016c75bf0f9f9b6e913e0c9ec84cb55bd15b9
Jason Barnett <jason.w.barnett@gmail.com>
2014-08-05 17:52:56 -0700

    removed some unused instance variables from Uptrends::ProbeGroup

--- lib/uptrends/probe_group.rb
+++ lib/uptrends/probe_group.rb
@@ -4,12 +4,10 @@ require 'uptrends/utils'
 module Uptrends
   class ProbeGroup
 
-    attr_reader   :original_hash, :original_keys, :attributes
+    attr_reader :original_hash, :attributes
 
     def initialize(probe_group_hash = {})
       @original_hash     = probe_group_hash
-      @original_keys     = probe_group_hash.keys
-      @attributes        = probe_group_hash.keys.map{|x| x.downcase.to_sym }
 
       Uptrends::Utils.gen_and_set_accessors(self)
     end
