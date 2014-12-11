3cf5d451c2b4889fe2495d3b6ca97a6b82a44e88
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-11 09:32:54 -0800

    added a few methods to enable/disable probes and/or their alerts.

--- lib/uptrends/probe.rb
+++ lib/uptrends/probe.rb
@@ -3,6 +3,26 @@ require 'uptrends/base'
 module Uptrends
   class Probe < Base
 
+    def enable
+      self.is_active = true
+      self.update!
+    end
+
+    def disable
+      self.is_active = false
+      self.update!
+    end
+
+    def enable_alerts
+      self.generate_alert = true
+      self.update!
+    end
+
+    def disable_alerts
+      self.generate_alert = false
+      self.update!
+    end
+
     private
     def api_url
       "/probes"
