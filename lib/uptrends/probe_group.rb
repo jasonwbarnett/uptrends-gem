ad7409548b4383f15829de8086d6e8250c304792
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-11 11:58:55 -0800

    changed what is returned when adding a probe to a probe group

--- lib/uptrends/probe_group.rb
+++ lib/uptrends/probe_group.rb
@@ -32,6 +32,16 @@ module Uptrends
       body = JSON.dump({"ProbeGuid" => probe.guid})
       response = @client.class.send(method, "#{api_url}/#{@guid}/members", body: body)
       self.class.check_error!(response)
+
+      parsed_response = response.parsed_response
+      if parsed_response.nil?
+        parsed_response
+      else
+        group_guid = parsed_response['Guid']
+        probe_guid = parsed_response['ProbeGuid']
+
+        [ @client.probe_group(group_guid), @client.probe(probe_guid) ]
+      end
     end
 
   end
