c6ab4aaa2abf75ab9ee553cdd54bab0cb210e562
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-11 10:46:50 -0800

    added Uptrends::ProbeGroup methods #add_probe and #members

--- lib/uptrends/probe_group.rb
+++ lib/uptrends/probe_group.rb
@@ -3,6 +3,24 @@ require 'uptrends/base'
 module Uptrends
   class ProbeGroup < Base
 
+    def add_probe(probe)
+      fail("You must pass an Uptrends::Probe")           unless Uptrends::Probe === probe
+      fail("The probe you passed does not have a guid.") unless probe.attributes.include?(:guid)
+      fail("This group does not have a guid.")           unless self.attributes.include?(:guid)
+
+      body = JSON.dump({"ProbeGuid" => probe.guid})
+      response = @client.class.post("#{api_url}/#{@guid}/members", body: body)
+      self.class.check_error!(response)
+    end
+
+    def members
+      fail("This group does not have a guid.")           unless self.attributes.include?(:guid)
+
+      response = @client.class.get("#{api_url}/#{@guid}/members")
+      self.class.check_error!(response)
+      Uptrends::Probe.parse(self, response)
+    end
+
     private
     def api_url
       "/probegroups"
