942d08981ba340a99f93fb0c45810f6ac851719c
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-10 23:28:48 -0800

    moved over checkpoints

--- lib/uptrends/checkpoint.rb
+++ lib/uptrends/checkpoint.rb
@@ -1,8 +1,21 @@
-require 'json'
-require 'uptrends/utils'
 require 'uptrends/base'
 
 module Uptrends
   class Checkpoint < Base
+    def self.parse(client, response)
+      checkpoints = super
+      if Array === checkpoints
+        checkpoints.map do |checkpoint|
+          new(client, response, checkpoint)
+        end
+      else
+        checkpoint = checkpoints
+        new(client, response, checkpoint)
+      end
+    end
+
+    def api_url
+      "/checkpointservers"
+    end
   end
 end
--- lib/uptrends/client.rb
+++ lib/uptrends/client.rb
@@ -2,8 +2,6 @@ require "httparty"
 require "uptrends/probe"
 require "uptrends/probe_group"
 require "uptrends/checkpoint"
-require "uptrends/utils"
-require "uptrends/api_error"
 
 module Uptrends
   class Client
@@ -25,87 +23,16 @@ module Uptrends
       self.class.headers({'Content-Type' => 'application/json', 'Accept' => 'application/json'})
     end
 
-    def probes(opts = {})
+    def probes
       get_probes
     end
 
-    def checkpoints(opts = {})
-      if opts[:refresh]
-        @checkpoints = get_checkpoints
-      else
-        @checkpoints ||= get_checkpoints
-      end
+    def checkpoints
+      get_checkpoints
     end
 
     def probe_groups
       @probe_groups ||= get_probe_groups
-    end
-
-    def get_probe_group_members(opts = {})
-      group = opts[:group]
-      fail("You must pass a probe group using group: option.") unless Uptrends::ProbeGroup === group
-      group_guid = opts[:group].guid ? opts[:group].guid : fail("The probe group you passed does not have a guid.")
-
-      res = self.class.get("/probegroups/#{group_guid}/members")
-      parsed_response = raise_or_return(res)
-      probe_group_members = parsed_response.inject([]) do |memo, x|
-        memo << Uptrends::Probe.new(x)
-        memo
-      end
-    end
-
-    def add_probe(opts = {})
-      probe = Uptrends::Probe.new(opts)
-      res  = self.class.post("/probes", body: Uptrends::Utils.gen_request_body(probe))
-      parsed_response = raise_or_return(res)
-      new_probe = Uptrends::Probe.new(parsed_response)
-
-      probes << new_probe
-
-      new_probe
-    end
-
-    def add_probe_to_group(opts = {})
-      probe = opts[:probe]
-      group = opts[:group]
-
-      fail("You must pass a probe and probe group using probe: and group: opts.") unless Uptrends::Probe === probe && Uptrends::ProbeGroup === group
-
-      probe_guid = opts[:probe].guid ? opts[:probe].guid : fail("The probe you passed does not have a guid.")
-      group_guid = opts[:group].guid ? opts[:group].guid : fail("The probe group you passed does not have a guid.")
-
-
-      post_body = JSON.dump({"ProbeGuid" => probe_guid})
-      res = self.class.post("/probegroups/#{group_guid}/members", body: post_body)
-      raise_or_return(res)
-    end
-
-    def update_probe(probe)
-      res = self.class.put("/probes/#{probe.guid}", body: Uptrends::Utils.gen_request_body(probe))
-      raise_or_return(res)
-    end
-
-    def delete_probe(probe)
-      res = self.class.delete("/probes/#{probe.guid}")
-      raise_or_return(res)
-
-      probes.delete_if { |x| x.guid == probe.guid }
-    end
-
-    def create_http_probe(opts = {})
-      name            = opts[:name]            ? opts[:name]            : fail("You must provide a name!")
-      url             = opts[:url]             ? opts[:url]             : fail("You must provide a URL!")
-      check_frequency = opts[:check_frequency] ? opts[:check_frequency] : 15
-      match_pattern   = opts[:match_pattern]
-
-      probe     = Uptrends::Probe.new(gen_new_probe_hash(name, url, check_frequency, match_pattern))
-      res  = self.class.post("/probes", body: Uptrends::Utils.gen_request_body(probe))
-      parsed_response = raise_or_return(res)
-      new_probe = Uptrends::Probe.new(parsed_response)
-
-      probes << new_probe
-
-      new_probe
     end
 
     private
@@ -151,16 +78,6 @@ module Uptrends
       base_hash.merge!({"MatchPattern"=>match_pattern}) unless match_pattern.nil?
 
       base_hash
-    end
-
-    def raise_or_return(result)
-      response_code = result.response.code.to_i
-      case response_code
-        when 200...300
-          result.parsed_response
-        else
-          raise Uptrends::ApiError.new(result.parsed_response)
-      end
     end
 
   end
